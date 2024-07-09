4b0d909481f8ad7e3fc187f5dfae2e20e2acbb5c
Mxhrshad <mserattalab@gmail.com>
2024-07-09 16:16:39 +0330

    creating boards

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const optionContainer = document.querySelector('.option-container');
 const flipButton = document.querySelector('#flip-button');
+const gameBoardsContainer = document.querySelector("#gameboards-container")
 
 
-
+// Option Coosing
 let angle = 0
 function flip(){
     const optionShips = (Array.from(optionContainer.children));
@@ -10,6 +11,17 @@ function flip(){
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`);
 }
 
+// Creating Boards
+const width = 10
 
+function createBoard(){
+    const gameBoardContainer = document.createElement('div');
+    gameBoardContainer.classList.add('game-board');
+    gameBoardContainer.style.backgroundColor = '#006994';
 
+    gameBoardsContainer.append(gameBoardContainer)
+}
+
+createBoard()
+createBoard()
 flipButton.addEventListener('click', flip);
