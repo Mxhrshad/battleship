7c8e47d472750bdaf98917d08ebdebc49d006adf
Mxhrshad <mserattalab@gmail.com>
2024-07-10 19:14:14 +0330

    Creating Ships

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ function flip(){
     const optionShips = (Array.from(optionContainer.children));
     angle = angle === 0 ? 90 : 0;
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`);
-}
+};
+flipButton.addEventListener('click', flip);
+
+
 
 // Creating Boards
 const width = 10;
@@ -32,4 +35,45 @@ function createBoard(user){
 
 createBoard('player');
 createBoard('computer');
-flipButton.addEventListener('click', flip);
+
+
+
+// Creating Ships
+class Ship {
+    constructor(name, length){
+        this.name = name;
+        this.length = length;
+    };
+};
+
+const destroyer = new Ship('destroyer', 2);
+const submarine = new Ship('submarine', 3);
+const cruiser = new Ship('cruiser', 3);
+const battleship = new Ship('battleship', 4);
+const carrier = new Ship('carrier', 5);
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier];
+
+function addShipPiece(ship){
+    const allBoardBlocks = document.querySelectorAll('#computer div');
+    let randomBoolean = Math.random() < 0.5;
+    let isHorizontal = randomBoolean;
+    let randomStartIndex = Math.floor(Math.random() * width * width);
+
+    let shipBlocks = [];
+
+    for (let i = 0; i < ship.length; i++){
+        if (isHorizontal){
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+        } else {
+            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width]);
+        };
+    };
+
+    shipBlocks.forEach(shipBlock => {
+        shipBlock.classList.add(ship.name);
+        shipBlock.classList.add('taken');
+    })
+};
+
+ships.forEach(ship => addShipPiece(ship));
